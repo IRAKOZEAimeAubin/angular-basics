11fe87a8770d5892e2e42de8a8c3e46630b7b67b
Irakoze Aime Aubin <irakozeaimeaubin@gmail.com>
2023-08-18 16:57:16 +0200

    feat: add new todo functionality

--- src/app/add-todo/add-todo.component.ts
+++ src/app/add-todo/add-todo.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { DataService } from '../core/data.service';
+import { Subscription } from 'rxjs';
+import { Todo } from '../types/todos';
 
 @Component({
   selector: 'ta-add-todo',
   templateUrl: './add-todo.component.html',
-  styleUrls: ['./add-todo.component.css']
+  styleUrls: ['./add-todo.component.css'],
 })
-export class AddTodoComponent {
+export class AddTodoComponent implements OnInit, OnDestroy {
+  sub!: Subscription;
+  tag!: string;
 
+  constructor(private dataService: DataService) {}
+
+  ngOnInit() {}
+
+  ngOnDestroy() {}
+
+  saveTodo(formValues: any): void {
+    let newTodo: Todo = <Todo>formValues;
+    newTodo.tags = this.tag.split(',');
+    console.log(newTodo);
+    this.sub = this.dataService.addTodo(newTodo).subscribe({
+      next: (todo) => console.log(todo),
+      error: (err) => console.log(err),
+    });
+  }
 }
--- src/app/core/data.service.ts
+++ src/app/core/data.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Todo } from '../types/todos';
 import { Observable } from 'rxjs';
@@ -15,5 +15,29 @@ export class DataService {
 
   getTodoById(id: string): Observable<Todo> {
     return this.http.get<Todo>(`http://localhost:8000/todos/${id}`);
+  }
+
+  addTodo(newTodo: Todo): Observable<Todo> {
+    return this.http.post<Todo>('http://localhost:8000/todos', newTodo, {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json',
+      }),
+    });
+  }
+
+  update(updatedTodo: Todo): Observable<void> {
+    return this.http.patch<void>(
+      `http://localhost:8000/todos/${updatedTodo.todoId}`,
+      updatedTodo,
+      {
+        headers: new HttpHeaders({
+          'Content-Type': 'application/json',
+        }),
+      }
+    );
+  }
+
+  delete(todoID: string): Observable<void> {
+    return this.http.delete<void>(`http://localhost:8000/todos/${todoID}`);
   }
 }
